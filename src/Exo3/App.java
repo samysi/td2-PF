73da05ca1c09f74e6eddf64773f41e63f2cba098
samysi <samysi2216@gmail.com>
2020-11-16 15:03:31 +0100

    Exo3, q1 à q6

--- src/Exo3/App.java
+++ src/Exo3/App.java
@@ -27,6 +27,118 @@ public class App {
 
     }
 
+    static Predicate<Etudiant> aDEF = new Predicate<Etudiant>() {
+        @Override
+        public boolean test(Etudiant etudiant) {
+
+            for (UE ue : etudiant.annee().ues()) {
+                for (Map.Entry<Matiere, Integer> ects : ue.ects().entrySet()) {
+
+                    Matiere matiere = ects.getKey();
+                    String note = etudiant.notes().containsKey(matiere) ? etudiant.notes().get(matiere).toString() : "DEF";
+                    if(note == "DEF"){
+                        return true;
+                    }
+
+                }
+            }
+            return false;
+
+        }
+    };
+
+    public static void exercise_3_question_2(Annee a){
+
+        afficheSi("**ETUDIANTS DEFAILLANT",aDEF , a);
+
+    }
+
+    static Predicate<Etudiant> aNoteEliminatoire = new Predicate<Etudiant>() {
+        @Override
+        public boolean test(Etudiant etudiant) {
+
+            for (Map.Entry<Matiere,Double> paire : etudiant.notes().entrySet()) {
+                if(paire.getValue() < 6){
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+
+    public static void exercise_3_question_3(Annee a){
+
+        afficheSi("**ETUDIANTS A NOTE ELIMINATOIRE",aNoteEliminatoire , a);
+
+    }
+
+    static Function<Etudiant, Double> moyenne = new Function<Etudiant, Double>() {
+        @Override
+        public Double apply(Etudiant e) {
+            if(aDEF.test(e)){
+                return null;
+            }else{
+
+                Double moy = 0.0;
+                Integer coef = 0;
+
+                for (UE ue : e.annee().ues()) {
+                    for (Map.Entry<Matiere, Integer> ects : ue.ects().entrySet()) {
+                        Matiere matiere = ects.getKey();
+                        Integer credits = ects.getValue();
+                        if(e.notes().containsKey(matiere)){
+                            Double note = e.notes().get(matiere);
+                            moy = moy + note * credits;
+                            coef = coef + credits;
+                        }
+
+                    }
+                }
+
+                moy = moy/coef;
+                return moy;
+            }
+        }
+    };
+
+    public static void exercise_3_question_4(Annee a){
+
+        System.out.println("**MOYENNES DES ETUDIANTS");
+        System.out.println("\n");
+
+        for (Etudiant e:a.etudiants()) {
+
+            System.out.println(moyenne.apply(e));
+
+        }
+    }
+
+    static Predicate<Etudiant> naPasLaMoyennev1 = n -> moyenne.apply(n) < 10;
+
+    public static void exercise_3_question_5(Annee a){
+
+        afficheSi("**ETUDIANTS SOUS LA MOYENNE",naPasLaMoyennev1,a);
+    }
+
+    static Predicate<Etudiant> naPasLaMoyennev2 = new Predicate<Etudiant>() {
+        @Override
+        public boolean test(Etudiant etudiant) {
+
+            if(moyenne.apply(etudiant) == null || moyenne.apply(etudiant) < 10){
+                return true;
+            }
+            return false;
+
+        }
+    };
+
+    public static void exercise_3_question_6(Annee a){
+
+        afficheSi("**ETUDIANTS SOUS LA MOYENNE (v2)",naPasLaMoyennev2,a);
+
+    }
+
+
 
     public static void main(String[] args){
 
@@ -53,6 +165,18 @@ public class App {
 
         exercise_3_question_1(a1);
 
+        exercise_3_question_2(a1);
+
+        exercise_3_question_3(a1);
+
+        exercise_3_question_4(a1);
+
+        System.out.println("\n");
+
+        //exercice_3_question_5(a1);
+        // On a une exception de type NullPointerException car un élève retourne une moyenne null et non un int
+
+        exercise_3_question_6(a1);
     }
 
 }
